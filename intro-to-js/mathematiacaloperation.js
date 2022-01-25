438c0a638ca646ac227e345a02d38459eb0b3d11
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-25 21:50:23 +0600

    math-shotcut

---
+++ intro-to-js/mathematiacaloperation.js
@@ -0,0 +1,21 @@
+var onionPrice = 43;
+var eggPrice = 42;
+eggPrice = 56;
+console.log(eggPrice);
+var eggQuantity = 7;
+var totalPrice = onionPrice+eggPrice;
+console.log(totalPrice);
+
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference)
+
+var eggPriceMultiplication =    eggQuantity * eggPrice;
+console.log(eggPriceMultiplication);
+
+
+var moneyAvailable = 100;
+var orrangePrice = 5
+
+var orrangeQuantitiy = moneyAvailable / orrangePrice;
+console.log(orrangeQuantitiy);
+
