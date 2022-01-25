836b77cbc64d871f83c7e4e8b8dc922b08970cdc
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-25 16:59:13 +0600

    mathOp add

--- intro-to-js/variable-name.js
+++ intro-to-js/variable-name.js
@@ -2,3 +2,22 @@
 
 var name = 'Hero Alam';
 
+// Rules of naming variable
+
+// cannt use keyword or reserved word to name a variable.
+//example :  var false = 15;.... Reserved keyword not allow
+
+// variable name should be one word or single word
+
+// example : var nayok name = 'hero alam';
+
+// quote is not allowed..... example :  var "naiyka" = 'shabnur';
+// camel case use korte hobe.. 
+
+var myOfficeAddress = 'andorkilla bandorbaan';
+
+var PIE = 3.14159;
+
+var price = 25;
+
+ 
