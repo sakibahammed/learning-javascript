2530a27ab7235c7843b01fbf4dec5bd90753f209
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-25 07:19:48 +0600

    variable decare about varible

--- intro-to-js/variable.js
+++ intro-to-js/variable.js
@@ -1,3 +1,23 @@
 // Variable in js
 
 // Variable is something that can vary
+
+var roll = 45;
+let address = 'dhaka';
+const name = 'Omar Ahammed Sakib';
+
+
+// Need 5 things to declare a variable : 
+
+// var/let/const variable_name = value ;
+
+
+var price = 21;
+
+let quantityCoke = 250;
+
+let age = 13;
+
+let temparature = 13;
+
+
