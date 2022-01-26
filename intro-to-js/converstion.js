3aaa0bdc4ee11f9357f99f4a653b80320b9cccbc
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-26 12:49:44 +0600

    finished

--- intro-to-js/converstion.js
+++ intro-to-js/converstion.js
@@ -3,3 +3,10 @@ var lastName = 'Kim';
 
 var fullName = firstName +' '+lastName;
 console.log(fullName)
+
+
+var onionPrice = '42';
+var eggPrice = '41';
+
+var totalprice = onionPrice + eggsPrice;
+console.log(totalprice)
