7c54fee8803046933312414ad6ea6c3308bea597
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-26 12:26:10 +0600

    int float parseint parsefloat

---
+++ intro-to-js/math-operation.js
@@ -0,0 +1,22 @@
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var multiplication = price1*price2;
+var minus = price1 - price2 
+var division = price1 / price2;
+// console.log(sum);
+// console.log(multiplication)
+// console.log(minus)
+// console.log(division)
+
+
+var doublePrice = price1 + 10;
+console.log(doublePrice);
+
+price2 +=10; //price2  = price2 + 10;
+
+price2++;
+console.log(price2);
+
+price2 --;
+
