7e570c48944eb98dff7ba1a4e9c5524acf4da883
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-27 15:38:06 +0600

    array

---
+++ JS fundamentals/array.js
@@ -0,0 +1,11 @@
+var friendsAge = [11,13,14,4,34];
+
+var murichanachurFee = [34,55,221,3];
+var naykas = ['mahi' ,'kopila' , 'opu' , 'porimoni'];
+
+var oddNumbers = [1,3,5,7,9];
+
+console.log(naykas)
+var vowels = ['a' , 'e','i','o','u'];
+
+
--- JS fundamentals/variable.js
+++ JS fundamentals/variable.js
@@ -1,1 +1,17 @@
+// Number
+
 var watchPrice = 120;
+watchPrice = 125;
+
+
+
+// String
+
+var userName = 'Sogir Uddin';
+
+//Boolean
+var isExpensive = true;
+
+//integer = 1 2 3 4 5 6
+                       
+//float = 2.3
