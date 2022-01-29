b2497b9c403384da92f6972f55859bb73db92fb8
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-30 04:30:58 +0600

    compjs

---
+++ JS fundamentals/comparison.js
@@ -0,0 +1,11 @@
+// console.log(5>6);
+// console.log(5<6);
+
+// console.log(5==6);
+
+
+console.log(5!=6);
+
+var romanticFeelings = 99;
+var herLove = 60;
+console.log(romanticFeelings == herLove);
