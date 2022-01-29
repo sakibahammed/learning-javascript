df7e87c7b8737202d74f3302c0d6fab8e670a40e
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-30 04:18:55 +0600

    pop

--- JS fundamentals/push-pop.js
+++ JS fundamentals/push-pop.js
@@ -1,4 +1,28 @@
 var lastBench = ['kalam','balam','salam'];
-// array te last index e kichu enter koranor jnne .push() use kora hoi.
+// array te last index e kichu enter koranor jnne .push() use kora hoi...new element last e entry kore and array er length barai.
 lastBench.push('jalam');
 console.log(lastBench);
+
+
+
+var lastBench = ['kalam','balam','salam'];
+lastBench.push('sadia');
+lastBench.push('palaam');
+lastBench.push('nalam');
+console.log(lastBench);
+
+var friendsAge = [11,13,17,12];
+friendsAge.push(14);
+console.log(friendsAge);
+
+
+
+// lastElement theke kono element soranor jnne pop use kora hoi -------   .pop();
+
+// remove an element from an array,
+
+var ourAge  = [12,11,14,15,13];
+ourAge.pop();
+console.log(ourAge)
+
+
