c1f80d2098794fe1c94267498d1d71afe44be832
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-30 07:13:02 +0600

    comparison.js

--- JS fundamentals/comparison.js
+++ JS fundamentals/comparison.js
@@ -8,4 +8,12 @@ console.log(5!=6);
 
 var romanticFeelings = 99;
 var herLove = 60;
-console.log(romanticFeelings == herLove);
+console.log(romanticFeelings < herLove);
+
+
+console.log(isBanker || goodStudent);
+
+
+
+
+
