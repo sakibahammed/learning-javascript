f7f542c5f382081c524e38862b2fcc9672642177
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-01-31 07:02:08 +0600

    multiplse-condition.js

---
+++ JS fundamentals/multiple-condition.js
@@ -0,0 +1,18 @@
+var gotJob = true;
+var moneySaved = 200000;
+var hasFlat = false;
+
+// if(gotJob == true && moneySaved>200000){
+//     console.log('cholo biye kore feli');
+// }
+// else{
+//     console.log('tor kopal e biye nai');
+// }
+
+
+if(gotJob == false || moneySaved>200000 || hasFlat ==true){
+    console.log('cholo biye kore feli');
+}
+else{
+    console.log('tor kopal e biye nai');
+}
