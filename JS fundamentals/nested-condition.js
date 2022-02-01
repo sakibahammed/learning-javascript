423b6c0ee3be9424c59053921c761107e878f7e9
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-02 02:08:18 +0600

    recap-loop

---
+++ JS fundamentals/nested-condition.js
@@ -0,0 +1,47 @@
+var danishPrice = 55;
+var butterBun = 25;
+var toastBiscuit = 10;
+var myBudget = 100;
+var packedGood = false;
+
+// nested e mainly shb kichu eksth hoi na eta ekta ladder er moto kaj kore
+
+if(danishPrice < myBudget){
+    if(packedGood ==true){
+        console.log('danish khabo')
+    }
+    else{
+        console.log('ami khawar age machi ordhek kheye felce');
+    }
+}
+
+else if(butterBun < myBudget){
+    if(packedGood ==true){
+        console.log('butterbun khabo')
+    }
+    else{
+        console.log('ami khawar age machi ordhek kheye felce');
+    }
+}
+
+else if(toastBiscuit< myBudget){
+    if(packedGood ==true){
+        console.log('toast khabo')
+    }
+    else{
+        console.log('ami khawar age machi ordhek kheye felce');
+    }
+}
+else{
+    console.log('batash er batash diye chaa khabo');
+}
+
+
+if(danishPrice < myBudget){
+    if(packedGood ==true){
+        console.log('danish khabo')
+    }
+    else{
+        console.log('ami khawar age machi ordhek kheye felce');
+    }
+}
---
+++ JS fundamentals/summery.js
@@ -0,0 +1,17 @@
+// variable declare korte 5 ta jinis laage - var/let/const variable name = variable value;
+
+var tableLength = 12;
+var tourDestination = ['CoxsBazar' , 'Kathmandu','vutan','paris'];
+console.log(tourDestination[1]);
+// array te k koi ace ta ber korar jnne index use korte hbe;
+tourDestination.indexOf('vutan');
+
+
+var fourthDestination = tourDestination[3];
+tourDestination[1] = 'srilanka';
+
+tourDestination.push('london');
+
+
+tourDestination.pop();
+
