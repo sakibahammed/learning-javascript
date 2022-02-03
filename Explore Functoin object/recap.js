4ab533f8405b359cb44337aa1c7f33cbcfe60a28
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-03 23:27:11 +0600

    recap of basic

---
+++ Explore Functoin object/recap.js
@@ -0,0 +1,35 @@
+var favouriteBook = '4 hour work weak';
+
+
+// array
+var bookList = ['positioning','hooked','start with why','shoe dog'];
+
+var shoedog = bookList.indexOf('shoe dog');
+bookList[1] = 'story Brand';
+bookList.push('small Giants');
+
+// booklist
+
+if(bookList[1]=='hooked'){
+    console.log('I am hooked');
+}
+else{
+    console.log('I am not hooked');
+}
+
+
+
+//loop
+
+//while loop 
+
+var i =0 ; // loop variable
+
+while(i<15){
+    console.log('looping loooping loooping');
+    i++;
+}
+
+for(var i = 0;i<10;i++){
+    console.log('I amm hooked hooked hooked');
+}
