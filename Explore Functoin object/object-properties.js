8e69e3fb9dde93ff0d59e3be26ce155ce8242010
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-04 22:41:13 +0600

    object

---
+++ Explore Functoin object/object-properties.js
@@ -0,0 +1,17 @@
+var computer = {
+    price : 29000,
+    Storage : '156gb',
+    processor : 'i5',
+    color : 'black'
+}
+
+// puro object ta dkhar jnne
+
+console.log(computer);
+
+// object er property dekhar jnne { objectName.propertyName}
+
+console.log(computer.processor);
+
+var computerPrice = computer.price;
+console.log(computerPrice);
---
+++ Explore Functoin object/object.js
@@ -0,0 +1,22 @@
+var student1 = {
+    name : 'Ashikul Islam',
+    id : 1209302,
+    marks : 81,
+    phoneNumber : 374838,
+    height : '5 feet '
+}
+
+var mobile = {
+    name : 'samsung',
+    color:'black',
+    screenSize : 5,
+    Storage: '16gb'
+}
+
+var book = {
+title : 'staart with why',
+author:'Simon sinek',
+price : 200,
+pages : 246,
+publisher : 'penguiune'
+}
