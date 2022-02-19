fdc21de3676f997a2007d126249fd22c6b04d49a
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-20 01:47:18 +0600

    case-convert.js

---
+++ js-string-array/case-convert.js
@@ -0,0 +1,13 @@
+const anthem = 'amar shonar bangla ami tomai valobasi';
+
+const searching = 'valo';
+
+const savedUserName ='BlackPink';
+const userInput = 'blackpink';
+
+if(userInput.toLowerCase()== savedUserName.toLocaleLowerCase()){
+    console.log('user exist');
+}
+ 
+
+// shob gula boro haat e korar jnne toUpperCase() use korte hoi
