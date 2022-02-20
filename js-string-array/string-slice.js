495f7899b017d0fb514c52c50e60a739730ba5f8
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-21 04:10:48 +0600

    split slice substr sbubstring concat join

--- js-string-array/string-slice.js
+++ js-string-array/string-slice.js
@@ -3,9 +3,44 @@ const anthem = 'amar shonar bangla ami tomai valonashi';
 const word = anthem.split(' ');
 const withoutA = anthem.split('a');
 
-console.log(withoutA);
+// console.log(withoutA);
 
-const smallSlice  = anthem.slice;
+
+
+//slice
+const smallSlice  = anthem.slice(5,13);
+// console.log(smallSlice);
+
+
+// substr()
+
+const anotherPart = anthem.substr(5,7);
+// console.log(anotherPart);
+
+
+// substirng 
+
+const anotherAnotherPart = anthem.substring(11,15);
+// console.log(anotherAnotherPart);
+
+
+const first = 'Amader';
+const second = 'City';
+
+const fullString = first.concat(second).concat('ABC').concat('rj kibria');
+console.log(fullString);
+
+
+
+const words2 = ['alim','badhon','cameron','david'];
+// const allJoined = words2.join(' ');
+// const allJoined = words2.join(' ');
+const allJoined = words2.join(', ');
+
+
+console.log(allJoined)
+
+
 
 
 
