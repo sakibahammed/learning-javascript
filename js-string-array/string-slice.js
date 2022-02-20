f37c42135e6c9af9e1b69f8ad0ad5b2374d3df11
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-20 22:09:03 +0600

    string slice.js

---
+++ js-string-array/string-slice.js
@@ -0,0 +1,35 @@
+const anthem = 'amar shonar bangla ami tomai valonashi';
+
+const word = anthem.split(' ');
+const withoutA = anthem.split('a');
+
+console.log(withoutA);
+
+const smallSlice  = anthem.slice;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
