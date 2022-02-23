e3957368a2068d125e949beb846ba463e6599d8f
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-23 14:05:38 +0600

    es6 intr

---
+++ es6/es6-intro/basic.js
@@ -0,0 +1,83 @@
+// variable declare
+
+var userDeposit = 400;
+
+const newDeposit = 200 // wont redeclare
+
+// condition
+
+if(newDeposit> 500){
+
+}
+else if(userDeposit < 200){
+
+}
+else if(userDeposit == 400){
+
+}
+else if(userDeposit !=400){
+
+}
+else{
+
+}
+
+
+// array 
+
+const numbers =  [45,554,34,23,22,122];
+// to find how many element in array ...    .length
+
+
+const numberCount = numbers.length;
+
+//array te notun element add korar jonne push() use kora hoi
+
+numbers.push(12);
+
+// array theke last element ber korar jonnne .pop() use korte hobe
+numbers.pop();
+
+// array er moddhe kono element change korar jonne 
+numbers[2] = 555;;; // number[2] holo array er element er position..and ota e redifne kora hocche
+
+// .indexOf()  kon positon e 
+
+
+// if included in array
+
+
+if(numbers.indexOf(11) !=1){
+
+}
+
+
+// if include
+
+
+if(numbers.includes(11)){
+
+}
+
+
+
+
+
+
+//loop
+
+for(const number of numbers){
+
+}
+
+
+// function
+
+
+function getFullName(){
+     const name = first + second ;
+
+}
+
+
+getFullName('rahim','ali');
---
+++ es6/let-const.js
@@ -0,0 +1,30 @@
+const balace = 1240 ;;;
+
+// cosnt diy kichu declare korl tar value direct change kora jai na .. 
+
+
+const userName = 'jan pakhi potash potash';
+
+const weTogether = userName + 'ami';
+console.log(weTogether);
+
+
+const numbers = [45,23,12,43,60];
+// numbers = [99,145,3];// numbers reasigned
+
+numbers.push(23);
+numbers[1] = 333,
+console.log(numbers);
+
+
+Note : // array or object declareation er somoi kono vabe e reaasign kora jaabe na . kintu ekta part niye kaaj kora jaabe
+
+const students = {
+    roll:101,
+    name : 'mofij',
+    job:'intern'
+}
+
+students.name = 'mofazzol';
+
+
