d90c197f3ccc274f74f9d480f4c94060d5d03817
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-24 17:15:47 +0600

    chainig destructuring in array chainig

---
+++ es6/more-es6/chainnign.js
@@ -0,0 +1,42 @@
+// declare variable based on the name of object property
+
+const myObject = { x: 2, y:50,z:600 , a : 25,b:68};
+const {x,y,b} = myObject;
+// console.log(myObject.p?.q);
+
+
+// destructiring er khetre object er property gula k e variable hesebe dhora hoi.
+
+
+// destructuring array 
+
+const [p,q] = [45,37,232,23,78,56,453];
+// console.log(p,q);
+
+
+
+const [best,faltu] = ['momtaz','porshi'];
+console.log(best , faltu);
+
+
+const {sky , color} = {sky : 'blue',soi:'mati',color:'red',money : 4500}
+
+//chaining
+
+const company = {
+    name: "GP",
+    ceo: { id: 1, name: "ajmol", food: "fuska" },
+    web: {
+      work: "website devlopment",
+      employee: 22,
+      framework: "react",
+      tech: {
+        first: "html",
+        second: "css",
+        third: "js",
+      },
+    },
+  };
+
+console.log(company?.web?.tech.third)
+// console.log(company?.back?.tech.third)
