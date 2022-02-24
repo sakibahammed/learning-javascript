7c1ee7cb3b4f2b43ceff663623f16953250f6e6c
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-24 22:53:52 +0600

    map.js

---
+++ es6/more-es6/map.js
@@ -0,0 +1,43 @@
+const numbers= [4,6,8,10];
+
+const output = [];
+
+const doubleIt = number => number*2;
+for(const number of numbers){
+    const result = doubleIt(number);
+    output.push(result);
+}
+
+// console.log(output);
+
+
+
+// { maapping
+
+// 1 . loop through each element
+// 2. for each element cll the provided function
+// 3. result for each element will be stored in an array 
+// }
+
+
+
+// const output2 = numbers.map(doubleIt);
+
+// const output2 = numbers.map(number => number *2);
+
+const output2 = numbers.map(x => x*2);
+console.log(output2);
+
+const square = numbers.map(x => x*x);
+console.log(square);
+
+
+
+
+// note : kono ekta array er upor loop chalano mane prottek ta element k loop chalai kichu ekta kora than seta k notun ekta array te push kore dea
+
+
+
+
+
+
