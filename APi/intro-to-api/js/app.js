11d116f2f8020781de590944a4fa59e08be434e3
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-25 11:36:09 +0600

    json , json structure, parse , strigify

--- APi/intro-to-api/js/app.js
+++ APi/intro-to-api/js/app.js
@@ -1,1 +1,32 @@
-console.log('api added');
+
+
+
+const user = { id : 11, name : 'Gorib ami',job:'actor'};
+const stringified = JSON.stringify(user);
+// console.log(stringified);
+
+// console.log(user);
+
+
+const shop ={
+    name : 'Alia store',
+    address : 'Ranbir store',
+    profit : 15000,
+    products : ['laptop','mobile','pepsi'],
+    owner : {
+        name : 'ALia Vaat',
+        profession : 'actor',
+    },
+    isExpensive : false
+}
+// convert to stirng
+const shopStringified = JSON.stringify(shop);
+console.log(shopStringified);
+// convert to javascript
+
+const converted = JSON.parse(shopStringified);
+console.log(converted.owner.name);
+
+
+
+
