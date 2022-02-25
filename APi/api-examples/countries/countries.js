cf3fb5d1e698733ac941a3faf8130d53b1502ef3
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-26 04:44:46 +0600

    call dynamic API , load dynamic data to display

--- APi/api-examples/countries/countries.js
+++ APi/api-examples/countries/countries.js
@@ -18,19 +18,44 @@ const displayCountries = countries => {
         
     // })
     for(country of countries){
-        console.log(country)
+        
 
         const div  = document.createElement('div');
         div.classList.add('country')
-        const h3 = document.createElement('h3');
-        h3.innerText = `Name :  ${country.name.common}`;
-        const p = document.createElement('p');
-        p.innerText = `Capital :  ${country.capital}
-        flag : ${country.flag}
-        map: ${country.maps.googleMaps}`;
-        div.appendChild(h3);
-        div.appendChild(p);
+        // const h3 = document.createElement('h3');
+        // h3.innerText = `Name :  ${country.name.common}`;
+        // const p = document.createElement('p');
+        // p.innerText = `Capital :  ${country.capital}
+        // flag : ${country.flag}
+        // map: ${country.maps.googleMaps}`;
+        // div.appendChild(h3);
+        // div.appendChild(p);
+        div.innerHTML = `
+            <h3>${country.name.common} </h3>
+            <p>${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name.common}')"> Details </button>
+        `
         
         countriesDiv.appendChild(div);
     }
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}
+    `
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+    // console.log(url);
+}
+
+const displayCountryDetail = country =>{
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv .innerHTML = `
+        <h3> ${country.name.common} </h3>
+        <h3> ${country.border} </h3>
+        <img src="${country.flags.png}" alt="">
+        <p> Population : ${country.population} </p>
+    `
+
 }
