a43340bd016f425a6effcd00225e2e0368921b59
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-26 01:39:04 +0600

    handel and display nested api data

---
+++ APi/api-examples/buddies/js/buddies.js
@@ -0,0 +1,31 @@
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(data => diplaybuddies(data))
+}
+
+loadBuddies();
+
+const diplaybuddies = data =>{
+    // const buddies = data.results;
+    // const buddiesDiv = document.getElementsByClassName('buddies');
+    // for(const buddy of buddies){
+    //     const p = document.createElement('p');
+    //     p.innerText = buddy.email;
+    //     buddiesDiv.appendChild(p);
+    // }
+
+
+    // console.log(data);
+    const buddies = data.results;
+    // console.log(buddies);
+    const buddyDiv = document.getElementById('buddies')
+    for(const buddy of buddies){
+        console.log(buddy.name.first);
+        const p = document.createElement('p');
+        p.innerText =  ` Name :${buddy.name.title} ${buddy.name.first} ${buddy.name.last} 
+        Email : ${buddy.email}`;
+        buddyDiv.appendChild(p);
+    }
+
+}
