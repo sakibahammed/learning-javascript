2ba747cb0f0d5feb2c8b0f66d460721fb0865ce6
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-26 23:46:41 +0600

    data-type.js note added

--- js-intersting-concepts/data-type.js
+++ js-intersting-concepts/data-type.js
@@ -4,6 +4,9 @@ let a = 'jamal uddin sahensah';
 console.log(typeof a);
 
 
+// Note : js k dyanmic type bole cause eta value dekhe ki type hobe ta thik kore.. eta type thik hoi value er upor based kore
+
+
 
 
 
