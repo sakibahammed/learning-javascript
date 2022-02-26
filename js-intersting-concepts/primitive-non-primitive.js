ebb3293bab70fa8b3a7cfc0af71849f49b00162c
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-27 01:38:07 +0600

    primitive -non premitive data type

---
+++ js-intersting-concepts/primitive-non-primitive.js
@@ -0,0 +1,57 @@
+/* 
+
+
+Data types : 
+
+
+primitive data types : 
+
+1 ==> number
+2 ==> string
+3 ==> boolean
+4 ==> undefined
+5 ==> null
+
+6 ==> symbol
+
+
+data types k 2 ta way te vaag korce 
+
+1. primitive data types ===> primary datas
+
+2. non-primitive data types ===> 
+
+
+1. Object
+*/
+
+let a = 'hello';
+
+let b = a;
+console.log(a,b);
+
+a = 'gello';
+console.log(a,b);
+
+//primitive data type holo immutable
+
+// primitive data type er easy example holo jgular single value thake ogula holo primitive data type....
+
+const x = {job:'web devloper'};
+const y = x;
+
+console.log(x,y)
+x.job = 'font-end-devloper';
+console.log(x,y)
+
+
+
+
+
+
+
+
+
+
+
+
