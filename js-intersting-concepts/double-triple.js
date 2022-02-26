3f5db7c4dd479306380888069401708b2c0adf0c
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-27 05:27:51 +0600

    double-triple.js

---
+++ js-intersting-concepts/double-triple.js
@@ -0,0 +1,54 @@
+const first = 1;
+const second = true;
+
+
+if(first == second){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+
+
+const a = {name : 'ali'};
+const b = {name : 'ali'};
+if(a==b){
+    console.log('both r same');
+
+}
+else{
+    console.log('they r not same');
+}
+
+
+
+
+
+
+
+/* 
+
+    ===================== Note ======================
+
+
+    doubleEqual checks value only dont go for data type and others .
+
+
+    tripleEqual checks data type . triple equal mainly checks if the value and values data type both are same
+
+
+    double equal chupi chupi shob check kore... convert kore than check kore. implicitly convert kore
+
+
+
+    avoid double equal ....
+
+    primitve gulo k double equal diye compare kora jai..
+    non-primitive gulo k double equal diye compare kora jai na
+
+
+    . primitive gulo k triple equal diye check kora jaai.. triple equal maan ta k check kore type check kore.. double equal convert korte jeye pechapechi kore fele
+
+
+*/
