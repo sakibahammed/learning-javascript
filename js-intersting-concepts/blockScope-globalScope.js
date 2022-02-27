9c0e0dbbc5c3ef69c659f3a44a82aba8aeb27861
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-02-27 16:13:50 +0600

    blockscope and global scop.js

---
+++ js-intersting-concepts/blockScope-globalScope.js
@@ -0,0 +1,58 @@
+function add(first ,second){
+    const result = first + second;
+
+    if (result > 9){
+        var mood = 'happy;'
+        // console.log(mood)
+    }
+    console.log(mood)
+    return result;
+}
+
+const sum = add(12,13);
+// console.log(result);
+
+
+
+
+
+
+
+
+/* 
+
+    function er vitore ekti variable toiri hoi tokhon setaake local scope bole. eti korar maddhome variable gulo k sudhu sei function er vitore access kora jaai//
+
+
+    let and const diye declare korle ta blockscope er under e porbe
+
+    Note : 
+
+    let and const block scope er moddhe thakbe
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
