3b5bfa7646984415a9102b9959bc8cecd17b4340
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-03-06 05:07:46 +0600

    timeout.js

---
+++ javascript-debug/timeOut.js
@@ -0,0 +1,32 @@
+function doSomething(){
+    console.log('i am coding javascript');
+}
+
+console.log('Iam first');
+console.log('I am second');
+// doSomething();
+setTimeout(doSomething)
+console.log('ami 3 number baccha');
+
+setTimeout(function(){
+    console.log('I am using vs code')
+});
+
+setTimeout(()=>{
+    console.log(' Ami mdn article porteci');
+})
+console.log('ami ek hali purno korci');
+
+
+
+
+// setTimeout() mainly holo sorkari officer jara main kaj pore korbe .. shey haat er kaaj korbe.. shb sesh e oi part taar kaaj korbe.... 
+
+// actually setTimeout() taar shob kaaj side e soraai and por er kaaj kore than taar kaj kore..... 
+
+// setTImeout er moddhe r ekta kaaj kora jaai taa holo etar moddhe koma diye ekta timeout declare kora jaai.. and eta milisecond e count hobe.....  
+
+// like setTimeout(function_name , time(mili second));
+
+
+// setTimeOut diye kono function k call korle seta synchronus vaabe hobe na. ulto seta asychronus vabe hobe
