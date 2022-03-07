72774680b10b3f01a793b16a8e714f9f3d445977
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-03-07 12:45:22 +0600

    update

---
+++ javascript-debug/javascript-event-loop.js
@@ -0,0 +1,12 @@
+/* 
+    first watch this video : https://www.youtube.com/watch?v=8aGhZQkoFbQ
+
+
+    read this web article : https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop
+
+    javascript e event loop naam er ekta jinis ace. eta run time er somoi shob gula calate thake... asynchronus er gulo side e rekh kaj korte thake than baki kaaj gula kore thaake.. full jinis ta k javascript ekta loop er mtoo kaaj kore.. ekta serial maintain kore.. que r stack er kaj kore... stack er kaj ta aage kore than que er kaj kore
+
+
+    
+    
+*/
