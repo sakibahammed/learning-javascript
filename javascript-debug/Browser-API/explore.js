61da40e18e57b0edd3738fa4bfa8bc2469e04c79
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-03-07 17:57:31 +0600

    browser api added

---
+++ javascript-debug/Browser-API/explore.js
@@ -0,0 +1,20 @@
+const name = 15;
+
+const student = {
+    name: 'jalii',
+    friend : 'borsha',
+    job:'business',
+    makeMovie : function(name){
+        console.log(name);
+    }
+}
+student.makeMovie('dinrat');
+document.getElementById('name'); 
+
+
+
+
+// read this article : 
+
+//  https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Introduction
+
--- javascript-debug/javascript-event-loop.js
+++ javascript-debug/javascript-event-loop.js
@@ -4,7 +4,7 @@
 
     read this web article : https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop
 
-    javascript e event loop naam er ekta jinis ace. eta run time er somoi shob gula calate thake... asynchronus er gulo side e rekh kaj korte thake than baki kaaj gula kore thaake.. full jinis ta k javascript ekta loop er mtoo kaaj kore.. ekta serial maintain kore.. que r stack er kaj kore... stack er kaj ta aage kore than que er kaj kore
+    javascript e event loop naam er ekta jinis ace. eta run time er somoi shob gula calate thake... asynchronus er gulo side e rekh kaj korte thake than baki kaaj gula kore thaake.. full jinis ta k javascript ekta loop er mtoo kaaj kore.. ekta serial maintain kore.. que r stack er kaj kore... stack er kaj ta aage kore than que er kaj kore... sequential ta stack and asynchronus ta que
 
 
     
