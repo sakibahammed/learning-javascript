0cb50a49ead0610a2ffaccb20a8a6a1c3f79835c
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-03-08 13:48:06 +0600

    alert confirm prompt

---
+++ javascript-debug/Browser-API/alert.js
@@ -0,0 +1,26 @@
+alert('I am here');
+alert('ma is coming with jharu')
+
+const maComing =()=>{
+    alert('ammu is coming open the book');
+}
+
+
+const askedPicnic = ()=>{
+  const response= confirm('r u going to picnic');
+  console.log(response);
+
+  if(response ==true){
+      alert('baksh the fee');
+  }
+  else{
+      alert('dgm');
+  }
+}
+
+
+
+const askName = ()=>{
+   const name =  prompt('what is your name');;
+   console.log(name);
+}
