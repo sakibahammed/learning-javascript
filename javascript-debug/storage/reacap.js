ddfcc794aaf36cca41e725e9f6e3d40dfbd412f3
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-03-10 02:35:58 +0600

    recap

---
+++ javascript-debug/storage/reacap.js
@@ -0,0 +1,34 @@
+// browser api
+
+alert('')
+confirm('are you single')
+prompt('name');
+
+
+//url 
+
+// location.reload()
+// location.hash = ''
+// //location.href = 
+
+
+
+//history.back()
+// history.go() => 0 diye reload , 1 diye ek step shamne , -1 diye pichabe
+
+//cookies ===> aage kaar din e manush jon data pathatw... coookies koma kore pathacche.. eta server e data pathate pare
+
+
+// contentEdible  ===== > html er kono content k edit korar jnne contentEditable use kora hoi... 
+
+document.body.contentEditable = true;
+
+
+// browser e new tab khular jnnne 
+
+// chrome://newtab
+// kothay k space khacche ta dekhar jnne ( shift  + esc ) use kora hoi 
+
+// chrome restart use korar jnne ; 
+
+// chrome : // restart
